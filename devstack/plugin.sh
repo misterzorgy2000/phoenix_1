a486e652ee06e113689ab0b7bd650e34ffdde2c0
misterzorgy <misterzorgy2000@yandex.ru>
2024-07-31 19:03:46 +0300

    Add devstack plugin

---
+++ devstack/plugin.sh
@@ -0,0 +1,49 @@
+function install_cow {
+    echo 'Install'
+}
+
+function init_cow {
+    echo 'Init'
+}
+
+function configure_cow {
+    echo 'Configure'
+}
+
+# check for service enabled
+if is_service_enabled template; then
+
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        # Set up system services
+        echo_summary "Configuring system services Template"
+        install_cow 
+
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of service source
+        echo_summary "Installing Template"
+        install_cow
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring Template"
+        configure_cow
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # Initialize and start the template service
+        echo_summary "Initializing Template"
+        init_cow
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down template services
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        :
+    fi
+fi
