b921db9281c0f39f19f983936ce1804101518138
misterzorgy <misterzorgy2000@yandex.ru>
2024-08-13 15:38:51 +0300

    update

--- cow/ai/main.py
+++ cow/ai/main.py
@@ -1,10 +1,17 @@
 from colorama import init, Fore
+from datetime import time, datetime
 
 init(autoreset=True)
 
 def run_ai():
     msg = "Hello from AI service"
     print(Fore.RED + msg)
+
+    while True:
+        with open("timestamp.txt", "a") as f:
+            f.write("Текущая временная метка: " + str(datetime.now()))
+            f.close()
+        time.sleep(10)
 
 if __name__ == '__main__':
     run_ai()
